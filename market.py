46d97bbe57e54937d1c0945179dd4e6bbf58bd28
PLOheMfW <meg4mag@gmail.com>
2021-04-09 14:35:38 +0300

    new type of run configuration

--- market.py
+++
@@ -1,7 +0,0 @@
-from flask import flask
-
-def main():
-    pass
-
-if __name__ =="__main__":
-    main()
---
+++ market/models.py
@@ -0,0 +1,19 @@
+from market import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(25), nullable=False,unique=True)
+    email_adress = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable = False,default=1000)
+    items = db.relationship('Item', backref='owner_user', lazy=True)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    description = db.Column(db.String(1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    def __repr__(self):
+        return "Item {}".format(self.name)
